5f06b85e7788dd86eb536a3456ec8f6c40b6b9eb
Analyn Cajocson <analyncajocson@ymail.com>
2019-10-19 19:01:32 +0800

    created feed endpoints for ph only

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -13,5 +13,21 @@ class Api::FeedsController < ApplicationController
     render json: response
   end
 
+  def philippines
+    feeds = History.where(country_name: 'Philippines')
+    filtered = feeds.uniq{|feed| feed.location}
+
+    response = filtered.map do |feed|
+      {
+        id: "item-#{feed.id}",
+        country_name: feed.country_name ,
+        location: feed.location ,
+        source: feed.source
+      }
+    end
+
+    render json: response
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     get 'feeds', to: 'feeds#index'
+    get 'feeds/philippines', to: 'feeds#philippines'
   end
 end
